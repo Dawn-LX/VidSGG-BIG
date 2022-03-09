40e34eb35e3f2efd4cb6677eebbff3166fb9ab47
gkf <kite_phone@zju.edu.cn>
2022-03-09 20:56:37 +0800

    initial commit

--- VidVRD-helper/prepare_gts_for_eval.py
+++ VidVRD-helper/prepare_gts_for_eval.py
@@ -9,8 +9,8 @@ from dataset import VidVRD, VidOR
 
 def prepare_gts_for_vidvrd():
     dataset = VidVRD(
-        anno_rpath = "/home/gkf/project/VidVRD-OpenVoc/datasets/vidvrd-dataset", 
-        video_rpath = '/home/gkf/project/VidVRD-OpenVoc/datasets/vidvrd-dataset/videos', 
+        anno_rpath = "/home/gkf/project/VidVRD_VidOR/vidvrd-dataset", 
+        video_rpath = '/home/gkf/project/VidVRD_VidOR/vidvrd-dataset/videos', 
         splits = ["train","test"],
     )
     indices = dataset.get_index(split="test")
@@ -23,12 +23,28 @@ def prepare_gts_for_vidvrd():
     with open(save_path,'w') as f:
         json.dump(video_level_gts,f)
 
+
 def prepare_gts_for_vidor():
-    pass
-    # TODO
+    dataset = VidOR(
+        anno_rpath = "/home/gkf/project/VidVRD_VidOR/vidor-dataset/annotation", 
+        video_rpath = '/home/gkf/project/VidVRD_VidOR/vidor-dataset/val_videos', # videos for val
+        # splits = ["training","validation"],
+        splits = ["validation"],
+        low_memory=False
+    )
+    indices = dataset.get_index(split="validation")
+
+
+    video_level_gts = dict()
+    for vid in indices:
+        video_level_gts[vid] = dataset.get_relation_insts(vid)
+    save_path = "datasets/GT_json_for_eval/VidORval_gts.json"
+    with open(save_path,'w') as f:
+        json.dump(video_level_gts,f)
 
 
 if __name__ == "__main__":
-    prepare_gts_for_vidvrd()
+    # prepare_gts_for_vidvrd()
+    prepare_gts_for_vidor()
     
     
