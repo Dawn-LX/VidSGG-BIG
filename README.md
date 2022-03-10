4ca337c0dcb8737fa3e88328a7d15b48b8f4ea25
gkf <kite_phone@zju.edu.cn>
2022-03-10 03:49:36 -0500

    initial commit

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ the code is still being organized
 
 # TODO 
 - add code for training
-- EvalFmtCvtor: remove the function for each part
 - add explanation for some term, e.g., "proposal" "use_pku"
+- Explain the EntiNameEmb and classeme and avg_clsme
+- explain the format of TrajProposal's feature, e.g., traj_classeme = traj_features[:,:,self.dim_feat:]
--- tools/eval_vidor2.py
+++ tools/eval_vidor2.py
@@ -1,56 +1,31 @@
 
-
 import root_path
-
 import pickle
 import json
+from tqdm import tqdm
 import os
 import argparse
-from tqdm import tqdm
 import torch
 
-from dataloaders.dataloader_vidvrd import Dataset,Dataset_pku,Dataset_pku_i3d
-from models import BIG_C_vidvrd
+from models import BIG_C_vidor
+from dataloaders.dataloader_vidor_v3 import Dataset
+# from dataloaders.dataloader_vidor import Dataset
 from utils.evaluate import EvalFmtCvtor
 from utils.utils_func import create_logger,parse_config_py,load_json
 from VidVRDhelperEvalAPIs import eval_visual_relation
 
 torch.set_printoptions(sci_mode=False,precision=4,linewidth=160)
 
-
-
-
-def load_checkpoint(model,optimizer,scheduler,ckpt_path):
-    checkpoint = torch.load(ckpt_path,map_location=torch.device('cpu'))
-    model.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optim_state_dict'])
-    scheduler.load_state_dict(checkpoint['sched_state_dict'])
-    crt_epoch = checkpoint["crt_epoch"]
-    batch_size = checkpoint["batch_size"]
-
-    return model,optimizer,scheduler,crt_epoch,batch_size
-
-def save_checkpoint(batch_size,crt_epoch,model,optimizer,scheduler,save_path):
-    checkpoint = {
-        "batch_size":batch_size,
-        "crt_epoch":crt_epoch + 1,
-        "model_state_dict":model.state_dict(),
-        "optim_state_dict":optimizer.state_dict(),
-        "sched_state_dict":scheduler.state_dict(),
-    }
-    torch.save(checkpoint,save_path)
-
-
 def inference_then_eval(
     cfg_path,
     weight_path,
     save_tag="",
-    use_pku=False,
+    use_regr = None,
     experiment_dir=None,
     gpu_id = 0,
-    save_infer_result=False,
     save_relation_json=False
 ):
+    
 
     device = torch.device("cuda:{}".format(gpu_id))
     
@@ -67,9 +42,17 @@ def inference_then_eval(
     dataset_config = all_cfgs["test_dataset_config"]
     model_config = all_cfgs["model_config"]
     infer_config = all_cfgs["inference_config"]
+    if use_regr is None:
+        use_regr = infer_config["use_regr"]
+    else:
+        infer_config["use_regr"] = use_regr
+    use_post=infer_config["use_post"]
     topk=infer_config["topk"]
 
     # create logger
+    # save_tag = "topk-{}_regr-{}_postpro-{}_epoch-{}".format(topk,use_regr,use_post,epoch) # this filename is too long
+    save_tag = "k{}_r{}_p{}_e{}".format(topk,use_regr,use_post,save_tag)
+
     log_path = os.path.join(log_dir,'eval_{}.log'.format(save_tag))
     logger = create_logger(log_path)
 
@@ -77,29 +60,24 @@ def inference_then_eval(
     logger.info(dataset_config)
     logger.info(infer_config)
 
-    model = BIG_C_vidvrd(model_config,is_train=False)
+    model = BIG_C_vidor(model_config,is_train=False)
 
     model = model.cuda(device)
     state_dict = torch.load(weight_path,map_location=torch.device('cpu'))
     state_dict = state_dict["model_state_dict"]
-    state_dict_ = {}
-    for k in state_dict.keys():
-        state_dict_[k[7:]] = state_dict[k]
-    # state_dict_ = replace_state_dict_keys(state_dict_)  ## for debug
-    model.load_state_dict(state_dict_)
+    temp = next(iter(state_dict.keys()))
+    if temp.startswith("module."):  # for model's state_dict saved after multi-gpu training
+        state_dict_ = {}
+        for k in state_dict.keys():
+            state_dict_[k[7:]] = state_dict[k]
+        model.load_state_dict(state_dict_)
+    else:
+        model.load_state_dict(state_dict)
     model.eval()
 
 
     ## construct dataset
-    if use_pku:
-        use_i3d = dataset_config.get("i3d_dir",None) is not None
-        if use_i3d:
-            dataset = Dataset_pku_i3d(**dataset_config)
-        else:
-            dataset = Dataset_pku(**dataset_config)        
-    else:
-        dataset = Dataset(**dataset_config)
-    
+    dataset = Dataset(**dataset_config)
     dataloader = torch.utils.data.DataLoader(
         dataset,
         batch_size=1,
@@ -113,18 +91,20 @@ def inference_then_eval(
     logger.info("infer_config:{}".format(infer_config))
     logger.info("weight_path:{}".format(weight_path))
 
-    convertor = EvalFmtCvtor("vidvrd")
-    predict_relations = {}
-    infer_result_for_save = {}
+    convertor_all = EvalFmtCvtor("vidor")
+    predict_relations = dict()
+    infer_result_for_save = dict()
     for proposal_list,gt_graph_list in tqdm(dataloader):
         proposal_list = [p.to(device) for p in proposal_list]
         gt_graph_list = [g.to(device) for g in gt_graph_list]
         with torch.no_grad():
-            batch_triplets = model(proposal_list,topk=topk)
+            batch_triplets = model(proposal_list,use_post=use_post,topk=topk,use_regr=use_regr)
 
+        
         assert len(proposal_list) == 1
         proposal = proposal_list[0].to(torch.device("cpu"))
         video_name = proposal_list[0].video_name
+
         if batch_triplets[0] is None:
             infer_result = None
             continue
@@ -132,22 +112,15 @@ def inference_then_eval(
         (
             uniq_quintuples,    # shape == (n_unique,5)
             uniq_scores,        # shape == (n_unique,3)
-            # uniq_pred_confs,    # shape == (n_unique,)
             uniq_dura_inters,   # shape == (n_unique,2) 
             uniq_query_ids,     # shape == (n_unique,)
         ) = batch_triplets[0]
         uniq_scores = torch.mean(uniq_scores,dim=-1)   # (n_unique,)
         infer_result = (uniq_quintuples.cpu(),uniq_scores.cpu(),uniq_dura_inters.cpu())
-        infer_result_for_save[video_name] = [x.cpu() for x in batch_triplets[0]]  # for debug
+        infer_result_for_save[video_name] = [x.cpu() for x in batch_triplets[0]]
 
-        pr_result = convertor.to_eval_format_pr(proposal,infer_result,use_pku=use_pku)
-        predict_relations.update(pr_result)
-
-    if save_infer_result:
-        save_path = os.path.join(experiment_dir,'VidVRDtest_infer_result_{}.pkl'.format(save_tag))
-        with open(save_path,'wb') as f:
-            pickle.dump(infer_result_for_save,f)
-        logger.info("infer_result saved at {}".format(save_path))
+        pr_result = convertor_all.to_eval_format_pr(proposal,infer_result,preserve_debug_info=True)
+        predict_relations.update(pr_result) 
 
     eval_relation(logger=logger,prediction_results=predict_relations)
     
@@ -158,8 +131,9 @@ def inference_then_eval(
             json.dump(predict_relations,f)
         logger.info("predict_relations have been saved at {}".format(save_path))
     logger.info("log file have been saved at {}".format(log_path))
+    
 
-
+ 
 def eval_relation(
     logger=None,
     prediction_results=None,
@@ -182,16 +156,17 @@ def eval_relation(
         assert json_results_path is None
     
 
-    gt_relations = load_json("datasets/GT_json_for_eval/VidVRDtest_gts.json")
+    gt_relations = load_json("datasets/GT_json_for_eval/VidORval_gts.json")
     mean_ap, rec_at_n, mprec_at_n = eval_visual_relation(gt_relations,prediction_results,viou_threshold=0.5)
     # logger.info(f"mAP:{mean_ap}, Retection Recall:{rec_at_n}, Tagging Precision: {mprec_at_n}")
     logger.info('detection mean AP (used in challenge): {}'.format(mean_ap))
     logger.info('detection recall: {}'.format(rec_at_n))
     logger.info('tagging precision: {}'.format(mprec_at_n))
 
+   
+
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description="Object Detection Demo")
     
     parser.add_argument("--cfg_path", type=str,help="...")
@@ -211,56 +186,58 @@ if __name__ == "__main__":
         )
     else:
         inference_then_eval(
-        args.cfg_path,
-        args.ckpt_path,
-        save_tag=args.save_tag,
-        use_pku=args.use_pku,
-        experiment_dir=args.output_dir,
-        gpu_id = args.cuda,
-        save_infer_result=False,
-        save_relation_json=False
+            args.cfg_path,
+            args.ckpt_path,
+            save_tag=args.save_tag,
+            experiment_dir=args.output_dir,
+            gpu_id = args.cuda,
+            save_relation_json=False
         )
+    
+
+
+    # inference_then_eval(cfg_path,weight_path,save_tag,use_regr=False,gpu_id=3,save_relation_result=False)
 
     '''
-    python tools/eval_vidvrd2.py \
-        --json_results_path /home/gkf/project/VideoGraph/training_dir_reorganized/vidvrd/model_0v10_pku_i3dclsme2_cachePKUv2/VidORval_predict_relations_topk10_pTrue_epoch70.json
+    ### table-3 BIG-C RoI
+    python tools/eval_vidor2.py \
+        --cfg_path experiments/exp4/config_.py \
+        --ckpt_path experiments/exp4/model_epoch_60.pth \
+        --save_tag epoch60_debug \
+        --cuda 1
+    022-03-10 15:54:44,476 - detection mean AP (used in challenge): 0.08030405719431091
+    2022-03-10 15:54:44,477 - detection recall: {50: 0.076040074, 100: 0.09395528}
+    2022-03-10 15:54:44,477 - tagging precision: {1: 0.6225961538461539, 5: 0.5096153934987692, 10: 0.4030048120766878}
+    2022-03-10 15:54:45,678 - log file have been saved at experiments/exp4/logfile/eval_k3_rTrue_pTrue_eepoch60_debug.log
+    '''
+
+
+    '''
+    ### table-3 BIG-C RoI + Lang
+    python tools/eval_vidor2.py \
+        --cfg_path experiments/exp5/config_.py \
+        --ckpt_path experiments/exp5/model_epoch_60.pth \
+        --save_tag epoch60_debug \
+        --cuda 1
     
-    ### exp1
-    python tools/eval_vidvrd2.py \
-        --cfg_path experiments/exp1/config_.py \
-        --ckpt_path /home/gkf/project/VideoGraph/training_dir_reorganized/vidvrd/model_0v10_cachePKUv1_rightcatid/model_epoch_80.pth \
-        --use_pku \
-        --cuda 1 \
-        --save_tag debug
-    
-    2022-03-09 02:41:18,550 - detection mean AP (used in challenge): 0.1756102305112229
-    2022-03-09 02:41:18,551 - detection recall: {50: 0.095966905, 100: 0.109203726}
-    2022-03-09 02:41:18,551 - tagging precision: {1: 0.565, 5: 0.4430000091344118, 10: 0.32350000478327273}
-    2022-03-09 02:41:18,668 - log file have been saved at experiments/exp1/logfile/eval_debug.log
-    
-    
-    ### exp2
-    python tools/eval_vidvrd2.py \
-        --cfg_path experiments/exp2/config_.py \
-        --ckpt_path /home/gkf/project/VideoGraph/training_dir_reorganized/vidvrd/model_0v10_pku_i3dclsme2_cachePKUv2/model_epoch_70.pth \
-        --use_pku \
-        --cuda 2 \
-        --save_tag debug
-    
-    2022-03-09 02:39:43,260 - detection mean AP (used in challenge): 0.17679591933675462
-    2022-03-09 02:39:43,261 - detection recall: {50: 0.09638056, 100: 0.11292658}
-    2022-03-09 02:39:43,261 - tagging precision: {1: 0.56, 5: 0.438000009059906, 10: 0.32850000374019145}
+    2022-03-10 15:15:34,062 - detection mean AP (used in challenge): 0.08296007380280078
+    2022-03-10 15:15:34,064 - detection recall: {50: 0.079225, 100: 0.09657621}
+    2022-03-10 15:15:34,064 - tagging precision: {1: 0.6442307692307693, 5: 0.5170673167404647, 10: 0.41052017033171767}
+    2022-03-10 15:15:35,310 - log file have been saved at experiments/exp5/logfile/eval_k3_rTrue_pTrue_eepoch60_debug.log
+
+    '''
+
+    ### table-3 BIG-C RoI + Lang
+    # model_class_path = "Tempformer_model/model_0v7.py"
+    # cfg_path = "training_dir_reorganized/vidor/model_0v7/config_.py"
+    # weight_path = "training_dir_reorganized/vidor/model_0v7/model_epoch_60.pth"
+    # save_tag = "epoch60_debug"
+    # 2022-03-09 21:04:59,378 - detection mean AP (used in challenge): 0.08296007380280078
+    # 2022-03-09 21:04:59,379 - detection recall: {50: 0.079225, 100: 0.09657621}
+    # 2022-03-09 21:04:59,379 - tagging precision: {1: 0.6442307692307693, 5: 0.5170673167404647, 10: 0.41052017033171767}
+    # 2022-03-09 21:04:59,390 - hit_infos have been saved at training_dir_reorganized/vidor/model_0v7/VidORval_hit_infos_k3_rFalse_pTrue_eepoch60_debug.pkl
+    # 2022-03-09 21:04:59,391 - evaluate results and log file have been saved at training_dir_reorganized/vidor/model_0v7/logfile/eval_k3_rFalse_pTrue_eepoch60_debug.log
+
 
     
-    ### exp3
-    python tools/eval_vidvrd2.py \
-        --cfg_path experiments/exp3/config_.py \
-        --ckpt_path /home/gkf/project/VidSGG-BIG/experiments/exp3/model_epoch_80.pth \
-        --cuda 3 \
-        --save_tag debug
     
-    2022-03-09 02:41:30,289 - detection mean AP (used in challenge): 0.26088200440572606
-    2022-03-09 02:41:30,289 - detection recall: {50: 0.14105481, 100: 0.16256464}
-    2022-03-09 02:41:30,289 - tagging precision: {1: 0.73, 5: 0.551, 10: 0.4}
-    2022-03-09 02:41:30,400 - log file have been saved at experiments/exp3/logfile/eval_debug.log
-    '''
