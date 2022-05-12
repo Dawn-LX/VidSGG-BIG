7391b4124d3a5bde53f61cde004d0fe54e5e60e1
gkf <kite_phone@zju.edu.cn>
2022-05-11 22:19:17 -0400

    update

---
+++ tools_draft/root_path.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os.path as osp
+import sys
+
+this_dir = osp.dirname(__file__)
+ROOT_PATH = osp.join(this_dir, '..')
+sys.path.append(ROOT_PATH)
